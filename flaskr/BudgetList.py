09d2da42d47001e4371c8781febab276758d8e40
ArielleWaks <ariellemwaks@gmail.com>
2024-08-21 22:08:13 -0500

    budgetlist class created

---
+++ flaskr/BudgetList.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import Expense
+
+class BudgetList():
+    def __init__(self, budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
+
+    def append(self, item):
+        if self.sum_expenses + item < self.budget:
+            self.expenses.append(item)
+            self.sum_expenses += item
+        else:
+            self.overages.append(item)
+            self.sum_overages += item
+
+    def __len__(self):
+        return len(self.expenses) + len(self.overages)
+    def __iter__(self):
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
+    def __next__(self):
+        try:
+            return self.iter_e.__next__()
+        except StopIteration as stop:
+            return self.iter_o.__next__()
+
+def main():
+    myBudgetList = BudgetList(1200)
+    expenses = Expense.Expenses()
+    expenses.read_expenses('/tmp/files/spending_data')
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+    print('Expenses count: ' + str(len(myBudgetList)))
+    for entry in myBudgetList:
+        print(entry)
+
+
+if __name__ == "__main__":
+    main()
